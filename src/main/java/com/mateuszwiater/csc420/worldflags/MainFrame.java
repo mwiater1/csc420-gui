31429465cf2e8c26e1306de92d75e555f8a9ac3e
Mateusz Wiater <Mateusz.Wiater@caci.com>
2016-11-08 09:51:14 -0500

    Initial commit.

---
+++ src/main/java/com/mateuszwiater/csc420/worldflags/MainFrame.java
@@ -0,0 +1,30 @@
+package com.mateuszwiater.csc420.worldflags;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.Dimension;
+import java.util.List;
+
+public class MainFrame extends JFrame {
+
+    MainFrame(final Dimension minSize, final List<Flag> flags) {
+        // Set the window parameters
+        setTitle("World Flags");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new MigLayout("","[grow,fill]","[grow,fill]"));
+
+        // Create the flag and selection panels
+        final FlagPanel flagPanel = new FlagPanel(minSize);
+        final SelectionPanel selectionPanel = new SelectionPanel(flagPanel, flags);
+
+        // Add the components to the window
+        add(flagPanel);
+        add(selectionPanel);
+
+        // Set the windows minimum size
+        pack();
+        setMinimumSize(getPreferredSize());
+    }
+}
